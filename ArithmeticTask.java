78bd2fde2ff7bc99da18861fdaa6870cab872daa
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-23 13:55:33 +0800

    1.1 update ArithmeticTask

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -3,5 +3,23 @@ public class ArithmeticTask {
 
     public static void main(String[] args) {
 
+        int x = 1, y = 2; // result is now 3
+        x = x += y;
+        System.out.println(x);
+
+        x = x -= 1; // result is now 2
+        System.out.println(x);
+
+        x = x *= 2; // result is now 4
+        System.out.println(x);
+
+        x = x /= 2; // result is now 2
+        System.out.println(x);
+
+        x = x += 8; // result is now 10
+        System.out.println(x);
+
+        x = x %= 7; // result is now 3
+        System.out.println(x);
     }
 }
